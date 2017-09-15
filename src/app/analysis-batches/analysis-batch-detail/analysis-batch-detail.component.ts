9653408551bcc24996bb12b34d8b37bf89ba9d14
Draper <bdraper@usgs.gov>
2017-09-15 18:37:56 -0500

    much progress on AB details component

--- src/app/analysis-batches/analysis-batch-detail/analysis-batch-detail.component.ts
+++ src/app/analysis-batches/analysis-batch-detail/analysis-batch-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { IAnalysisBatchSummary } from '../analysis-batch-summary';
 import { IAnalysisBatch } from '../analysis-batch';
+import { IExtraction } from '../../SHARED/extraction';
+import { IInhibition } from '../../SHARED/inhibition';
+import { IReverseTranscription } from '../../SHARED/reverse-transcription';
 
 import { AnalysisBatchService } from '../analysis-batch.service';
 
@@ -12,18 +15,44 @@ import { AnalysisBatchService } from '../analysis-batch.service';
 })
 export class AnalysisBatchDetailComponent implements OnInit {
   @Input() selectedABSummary: IAnalysisBatchSummary;
-  
-      loading: boolean;
-      selectedABDetail: IAnalysisBatch;
+
+  loading: boolean;
+  selectedABDetail: IAnalysisBatch;
+
+  extractionDetailArray: IExtraction[] = [];
+  inhibitionDetailArray: IInhibition[] = [];
+  rtDetailArray: IReverseTranscription[] = [];
+  targetDetailArray;
 
   constructor(private _analysisBatchService: AnalysisBatchService) { }
 
   ngOnInit() {
     this.loading = true;
 
-      this.selectedABDetail = this._analysisBatchService.getAnalysisBatchData(this.selectedABSummary.id);
-      console.log(this.selectedABDetail);
+    this.selectedABDetail = this._analysisBatchService.getAnalysisBatchData(this.selectedABSummary.id);
+    //console.log(this.selectedABDetail);
+
+    this.extractionDetailArray = this.selectedABDetail.extractions;
+
+    //build the inhibition list by looping through the AB data and adding all inhibitions to a local array
+    for (let extraction of this.extractionDetailArray) {
+      for (let inhibition of extraction.inhibitions) {
+        this.inhibitionDetailArray.push(inhibition)
+      }
+    }
+
+     //build the inhibition list by looping through the AB data and adding all inhibitions to a local array
+     for (let extraction of this.extractionDetailArray) {
+      for (let rt of extraction.reverse_transcriptions) {
+        this.rtDetailArray.push(rt)
+      }
+    }
+
+
+    this.loading = false;
 
   }
+
+
 
 }
