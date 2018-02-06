5b32c8eb742f501e857f2de8ead2067ad8ddf653
Tonia Roddick <troddick@usgs.gov>
2018-02-06 09:10:32 -0600

    labels ready for testing

--- src/app/samples/sample-label/sample-label.component.ts
+++ src/app/samples/sample-label/sample-label.component.ts
@@ -2,57 +2,6 @@ import { Component, OnInit, ViewChild, ElementRef, TemplateRef } from '@angular/
 import { SampleService } from '../../samples/sample.service';
 
 declare let jsPDF: any;
-//declare let html2pdf: any;
-
-// used for right-aligning the text added to pdf
-var splitRegex = /\r\n|\r|\n/g;
-jsPDF.API.textEx = function (text: any, x: number, y: number, hAlign?: string, vAlign?: string) {
-	var fontSize = this.internal.getFontSize()
-		/ this.internal.scaleFactor;
-
-	// As defined in jsPDF source code
-	var lineHeightProportion = 1.15;
-
-	var splittedText: string[];
-	var lineCount: number = 1;
-	if (vAlign === 'middle' || vAlign === 'bottom'
-		|| hAlign === 'center' || hAlign === 'right') {
-
-		splittedText = typeof text === 'string'
-			? text.split(splitRegex)
-			: text;
-
-		lineCount = splittedText.length || 1;
-	}
-
-	// Align the top
-	y += fontSize * (2 - lineHeightProportion);
-
-	if (vAlign === 'middle') y -= (lineCount / 2) * fontSize;
-	else if (vAlign === 'bottom') y -= lineCount * fontSize;
-
-
-	if (hAlign === 'center'
-		|| hAlign === 'right') {
-
-		var alignSize = fontSize;
-		if (hAlign === 'center') alignSize *= 0.5;
-
-		if (lineCount > 1) {
-			for (var iLine = 0; iLine < splittedText.length; iLine++) {
-				this.text(splittedText[iLine],
-					x - this.getStringUnitWidth(splittedText[iLine]) * alignSize,
-					y);
-				y += fontSize;
-			}
-			return this;
-		}
-		x -= this.getStringUnitWidth(text) * alignSize;
-	}
-
-	this.text(text, x, y);
-	return this;
-};
 
 @Component({
 	selector: 'sample-label',
@@ -60,8 +9,14 @@ jsPDF.API.textEx = function (text: any, x: number, y: number, hAlign?: string, v
 	styleUrls: ['./sample-label.component.scss']
 })
 export class SampleLabelComponent implements OnInit {
+	@ViewChild('labelTable1') labelTable1: ElementRef;
+	@ViewChild('labelTable2') labelTable2: ElementRef;
+	@ViewChild('labelTable3') labelTable3: ElementRef;
 	public labelAliquots: Array<any>;
 	private labelSkips: number;
+	public labelTable1Rows: any[];
+	public labelTable2Rows: any[];
+	public labelTable3Rows: any[];
 
 	constructor(private _sampleServices: SampleService) {
 	}
@@ -69,113 +24,121 @@ export class SampleLabelComponent implements OnInit {
 	ngOnInit() {		
 		this.labelAliquots = [];
 		this.labelSkips = 0;
+		this.labelTable1Rows =[];
+		this.labelTable2Rows =[];
+		this.labelTable3Rows =[];
+
+		// when they click 'createLabelPDF' from Samples, the label parts are stored in the sample.service.ts and subscribed to from here
 		this._sampleServices.LabelParts.subscribe(lp => {
 			this.labelAliquots = [];
 			this.labelSkips = lp[0];
+
+			// account for any skipped cells
 			let x = 0;
 			while (x < this.labelSkips) {
 				this.labelAliquots.push({"aliquot_string": "", "collaborator_sample_id": ""});
 				x++;
 			}
-/*			this.labelAliquots = this.fakeLabelArray();*/
+			
+			// if include is ture, add it to the array for this preview table of labels
 			lp[1].forEach(element => {
 				if (element.include)
 					this.labelAliquots.push(element);
 			});
+	//		this.labelAliquots = this.fakeLabelArray();
 		});
 	}
 	
-	public printLabelPDF() {
-		let pdf = new jsPDF('p', 'pt', 'a4', true);		
+	// print the labels
+	public printLabelPDF() { //'p', 'pt', 'a4', true
+		let pdf = new jsPDF('p','in','letter');
 		pdf.setFontSize(8);
 		pdf.setFontType("normal");
-		// depending on where we start (labelSkips = number of labels down to skip)
-		/*pdf.text(10, 20, new Date().toLocaleDateString());
-
-		pdf.setFontSize(10);
-		pdf.setFontType("bold");
-		if (this.abWorksheet.isReprint) {
-			pdf.text(200, 20, 'Analysis Batch Worksheet Reprint');
-		} else {
-			pdf.text(200, 20, 'Analysis Batch Worksheet');
+		
+		this.labelTable1Rows = this.tableToJson(this.labelTable1.nativeElement);
+		this.labelTable1Rows.forEach((r, i) => {
+			r['index'] = i + 1;
+		});
+		let Columns = ["","","","",""];
+		pdf.autoTable(Columns, this.labelTable1Rows, {
+			startY: 0.24,
+			margin: [0.6, 0.6, .1, 0.6], //[top, right, bottom, left]
+			theme: 'plain',
+			drawHeaderRow:  function() {return false;},
+			styles: {
+				halign: 'center', valign: 'middle', cellPadding: 0.153
+			},
+			columnStyles: {
+				0: { columnWidth: 1.43 },
+				1: { columnWidth: 1.43 },
+				2: { columnWidth: 1.43 },
+				3: { columnWidth: 1.43 },
+				4: { columnWidth: 1.43 }
+			}		
+		});
+		// if second table is populated
+		if (this.labelTable2) {
+			pdf.addPage();
+			this.labelTable2Rows = this.tableToJson(this.labelTable2.nativeElement);
+			this.labelTable2Rows.forEach((r, i) => {
+				r['index'] = i + 1;
+			});
+			let Columns2 = ["","","","",""];
+			pdf.autoTable(Columns2, this.labelTable2Rows, {
+				startY: 0.22,
+				margin: [0.6, 0.6, .1, 0.6], //[top, right, bottom, left]
+				theme: 'plain',
+				drawHeaderRow:  function() {return false;},
+				styles: {
+					halign: 'center', valign: 'middle', cellPadding: 0.152
+				},
+				columnStyles: {
+					0: { columnWidth: 1.32 },
+					1: { columnWidth: 1.32 },
+					2: { columnWidth: 1.32 },
+					3: { columnWidth: 1.32 },
+					4: { columnWidth: 1.32 }
+				}		
+			});
 		}
-		pdf.setFontSize(8);
-		pdf.setFontType("bold");
-
-		// .textEx(string, over, down, )
-		// Top left Table /////////////////////////////
-		pdf.textEx('Analysis batch:', 70, 50, 'right', 'middle');
-		pdf.setFontType("normal");
-		pdf.textEx(this.abWorksheet.analysis_batch.toString(), 75, 50, 'left', 'middle');
-		pdf.setFontType("bold");
-		pdf.textEx('Creation date:', 70, 60, 'right', 'middle');
-		pdf.setFontType("normal");
-		pdf.textEx(this.abWorksheet.creation_date, 75, 60, 'left', 'middle');
-
-		// split the studies comma separated string in case too long.
-		let studySplit = pdf.splitTextToSize(this.abWorksheet.str_studies, 240);
-		let studyRows = studySplit.length;
-
-		
-		// END Top left Table /////////////////////////////
-
-		// Top right Table /////////////////////////////
-		pdf.setFontType("bold");
-		pdf.textEx('Extraction No.:', 405, 50, 'right', 'middle');
-		pdf.setFontType("normal");
-		pdf.textEx(this.abWorksheet.extraction_no.toString(), 410, 50, 'left', 'middle');
-
-		pdf.setFontType("bold");
-		pdf.textEx('Extraction date:', 405, 60, 'right', 'middle');
-		pdf.setFontType("normal");
-		pdf.textEx(this.abWorksheet.extraction_date, 410, 60, 'left', 'middle');
-		pdf.setFontType("bold");
-		pdf.textEx('Extraction sample volume:', 560, 60, 'right', 'middle');
-		pdf.setFontType("normal");
-		pdf.textEx(this.abWorksheet.extraction_sample_volume.toString(), 565, 60, 'left', 'middle');
-
-		pdf.setFontType("bold");
-		pdf.textEx('Extraction method:', 405, 70, 'right', 'middle');
-		pdf.setFontType("normal");
-		pdf.textEx(this.abWorksheet.extraction_method["name"], 410, 70, 'left', 'middle');
-		pdf.setFontType("bold");
-		pdf.textEx('Eluted extracted volume:', 560, 70, 'right', 'middle');
-		pdf.setFontType("normal");
-		pdf.textEx(this.abWorksheet.eluted_extraction_volume.toString(), 565, 70, 'left', 'middle');
-		// END Top right Table /////////////////////////////
-	
-		
-		pdf.textEx('Reverse transcription No.:', 110, nextNewStartingY + 20, 'right', 'middle');
-		pdf.textEx(this.abWorksheet.reverse_extraction_no.toString(), 115, nextNewStartingY + 20, 'left', 'middle');
-
-		pdf.textEx('RT reaction volume:', 110, nextNewStartingY + 30, 'right', 'middle');
-		pdf.textEx(this.abWorksheet.rt_reaction_volume.toString(), 115, nextNewStartingY + 30, 'left', 'middle');
-
-		/*pdf.textEx('qPCR replicates:', 110, nextNewStartingY + 40, 'right', 'middle');
-		pdf.textEx('---', 115, nextNewStartingY + 40, 'left', 'middle');*
-
-		pdf.text('Notes:', 20, nextNewStartingY + 70);
-		pdf.line(15, nextNewStartingY + 75, 580, nextNewStartingY + 75);
-				
-		pdf.save("worksheet.pdf");*/
+		// if third table is populated
+		if (this.labelTable3) {
+			pdf.addPage();
+			this.labelTable3Rows = this.tableToJson(this.labelTable3.nativeElement);
+			this.labelTable3Rows.forEach((r, i) => {
+				r['index'] = i + 1;
+			});
+			let Columns3 = ["","","","",""];
+			pdf.autoTable(Columns3, this.labelTable3Rows, {
+				startY: 0.22,
+				margin: [0.6, 0.6, .1, 0.6], //[top, right, bottom, left]
+				theme: 'plain',
+				drawHeaderRow:  function() {return false;},
+				styles: {
+					halign: 'center', valign: 'middle', cellPadding: 0.152
+				},
+				columnStyles: {
+					0: { columnWidth: 1.32 },
+					1: { columnWidth: 1.32 },
+					2: { columnWidth: 1.32 },
+					3: { columnWidth: 1.32 },
+					4: { columnWidth: 1.32 }
+				}		
+			});
+		}
+		pdf.save("aliquot_labels.pdf");
 	}
 
 	// convert table element to json for the .autoTable()
 	private tableToJson(table) {
 		let data = [];
 
-		// first row needs to be headers
-		let headers = [];
-		for (let i = 0; i < table.rows[0].cells.length; i++) {
-			headers[i] = table.rows[0].cells[i].innerHTML;
-		}
-
 		// go through cells
-		for (let i = 1; i < table.rows.length; i++) {
+		for (let i = 0; i < table.rows.length; i++) {
 			let tableRow = table.rows[i];
 			let rowData = {};
 			for (let j = 0; j < tableRow.cells.length; j++) {
-				rowData[headers[j]] = tableRow.cells[j].innerHTML;
+				rowData[j] = tableRow.cells[j].innerText;
 			}
 			data.push(rowData);
 		}
@@ -194,8 +157,7 @@ export class SampleLabelComponent implements OnInit {
 		while (i < 85) {
 			fakeLabelArr.push({"aliquot_string": `test ${i+1}`, "collaborator_sample_id": i+1 });
 			i++;
-		}
-			
+		}			
 		return fakeLabelArr;
 	}
 
