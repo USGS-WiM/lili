e6d37c8730b104d5c5f266f44a3ae34a2ee74a98
Draper <bdraper@usgs.gov>
2019-03-14 15:11:13 -0500

    fixes to results page and data upload fixes. No dist build.

--- src/app/data-upload/data-upload.component.ts
+++ src/app/data-upload/data-upload.component.ts
@@ -278,27 +278,48 @@ export class DataUploadComponent implements OnInit {
 
     for (let rep of rawResults) {
       if (rep.Name === "EXT NEG") {
-        rawTargetResults.ext_neg_cq_value = Number(rep.Cp);
-        rawTargetResults.ext_neg_gc_reaction = Number(rep.Concentration);
+        if (rep.Cp === "") { rawTargetResults.ext_neg_cq_value = null; } else { rawTargetResults.ext_neg_cq_value = Number(rep.Cp) }
+        // tslint:disable-next-line:max-line-length
+        if (rep.Concentration === "") { rawTargetResults.ext_neg_gc_reaction = null; } else { rawTargetResults.ext_neg_gc_reaction = Number(rep.Cp) }
+
+        // rawTargetResults.ext_neg_cq_value = Number(rep.Cp);
+        // rawTargetResults.ext_neg_gc_reaction = Number(rep.Concentration);
       }
       if (rep.Name === "PCR NEG") {
-        rawTargetResults.pcr_neg_cq_value = Number(rep.Cp);
-        rawTargetResults.pcr_neg_gc_reaction = Number(rep.Concentration);
+        if (rep.Cp === "") { rawTargetResults.pcr_neg_cq_value = null; } else { rawTargetResults.pcr_neg_cq_value = Number(rep.Cp) }
+        // tslint:disable-next-line:max-line-length
+        if (rep.Concentration === "") { rawTargetResults.pcr_neg_gc_reaction = null; } else { rawTargetResults.pcr_neg_gc_reaction = Number(rep.Cp) }
+
+        // rawTargetResults.pcr_neg_cq_value = Number(rep.Cp);
+        // rawTargetResults.pcr_neg_gc_reaction = Number(rep.Concentration);
       }
       if (rep.Name === "POS") {
-        rawTargetResults.pcr_pos_cq_value = Number(rep.Cp);
-        rawTargetResults.pcr_pos_gc_reaction = Number(rep.Concentration);
+        if (rep.Cp === "") { rawTargetResults.pcr_pos_cq_value = null; } else { rawTargetResults.pcr_pos_cq_value = Number(rep.Cp) }
+        // tslint:disable-next-line:max-line-length
+        if (rep.Concentration === "") { rawTargetResults.pcr_pos_gc_reaction = null; } else { rawTargetResults.pcr_pos_gc_reaction = Number(rep.Cp) }
+
+        // rawTargetResults.pcr_pos_cq_value = Number(rep.Cp);
+        // rawTargetResults.pcr_pos_gc_reaction = Number(rep.Concentration);
       }
       if (rep.Name === "RT NEG") {
-        rawTargetResults.rt_neg_cq_value = Number(rep.Cp);
-        rawTargetResults.rt_neg_gc_reaction = Number(rep.Concentration);
+        if (rep.Cp === "") { rawTargetResults.rt_neg_cq_value = null; } else { rawTargetResults.rt_neg_cq_value = Number(rep.Cp) }
+        // tslint:disable-next-line:max-line-length
+        if (rep.Concentration === "") { rawTargetResults.rt_neg_gc_reaction = null; } else { rawTargetResults.rt_neg_gc_reaction = Number(rep.Cp) }
+
+        // rawTargetResults.rt_neg_cq_value = Number(rep.Cp);
+        // rawTargetResults.rt_neg_gc_reaction = Number(rep.Concentration);
       }
 
       if (numbersOnlyPattern.test(rep.Name)) {
+
+        // these two lines set blank values to null, and coerce non-blank values to numbers
+        if (rep.Cp === "") { rep.Cp = null; } else { rep.Cp = Number(rep.Cp) }
+        if (rep.Concentration === "") { rep.Concentration = null; } else { rep.Concentration = Number(rep.Cp) }
+
         rawTargetResults.updated_pcrreplicates.push({
           "sample": Number(rep.Name),
-          "cq_value": Number(rep.Cp),
-          "gc_reaction": Number(rep.Concentration)
+          "cq_value": rep.Cp,
+          "gc_reaction": rep.Concentration
         });
       }
     }
